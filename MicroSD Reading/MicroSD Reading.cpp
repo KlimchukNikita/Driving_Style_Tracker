548b1de0f1c70af5e8efb4cd4292adc8e73efd2a
KlimchukNikita <KlimchukNik@gmail.com>
2020-10-21 20:10:14 +0300

    MicroSD Reading added

---
+++ MicroSD Reading/MicroSD Reading.cpp
@@ -0,0 +1,34 @@
+#include "SPI.h"
+#include "SD.h"
+
+const int chipSelect = 4;
+
+void setup()
+{
+    Serial.begin(9600);
+
+    if (!SD.begin(chipSelect))
+    {
+        Serial.println("initialization failed!");
+        return;
+    }
+
+    File myFile = SD.open("test.txt");
+
+    if (myFile)
+    {
+        while (myFile.available())
+        {
+            Serial.write(myFile.read());
+        }
+
+        myFile.close();
+    }
+
+    else
+    {
+        Serial.println("error opening test.txt");
+    }
+}
+
+void loop() {}
